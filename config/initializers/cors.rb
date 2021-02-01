cb92c9c7f4fab95944b3cea8eb29ba91b6ed3264
gaaamii <i1shi4ga4mi1@gmail.com>
2021-02-01 22:03:40 +0900

    Revert "add localhost to client origins"
    This reverts commit 68ba5f33726b23aac20a7314bdb976c443d2c1f2.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins [ENV["BLOG_SERVER_ORIGIN"], "http://localhost:3000"]
+    origins ENV["BLOG_SERVER_ORIGIN"]
 
     resource '/user_sessions/*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     resource '/posts/*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
