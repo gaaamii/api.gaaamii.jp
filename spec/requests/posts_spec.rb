6d2daf53aa8eb3c2b19d956fe2a7347bbe03c112
gaaamii <i1shi4ga4mi1@gmail.com>
2022-06-27 11:24:27 +0900

    Add a spec to (POST) /posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,11 +1,33 @@
 require "rails_helper"
 
 RSpec.describe PostsController, type: :request do
-  describe "#index" do
+  include Requests::LoginHelper
+
+  describe "(GET) /posts" do
     it "returns an index of posts" do
       get "/posts"
       expect(response).to have_http_status(:ok)
       expect(JSON.parse(response.body)).to be_kind_of(Array)
+    end
+  end
+
+  describe "(POST) /posts" do
+    let(:user) { FactoryBot.create(:user) }
+    before { login(user.email, 'test_password') }
+
+    let(:post_params) do
+      {
+        post: {
+          title: 'Title',
+          body: 'body',
+          published_at: Time.now.iso8601,
+        }
+      }
+    end
+
+    it "returns 201 http status" do
+      post "/posts", params: post_params
+      expect(response).to have_http_status(:created)
     end
   end
 end
