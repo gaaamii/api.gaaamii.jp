6f11409121030ca939bf87ae35ec98e5256439e8
gaaamii <i1shi4ga4mi1@gmail.com>
2022-12-28 13:25:00 +0900

    add Cors path

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,6 +11,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '/user_sessions/*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     resource '/posts/*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    resource '/admin/posts/*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     resource '/cloudinary_signature', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
