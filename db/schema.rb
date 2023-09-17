8003eb6a463050a9f1ea6c7b90a1b40fdeb66c43
gaaamii <i1shi4ga4mi1@gmail.com>
2023-09-17 22:07:40 +0900

    Add Link model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2021_12_24_124812) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_17_130435) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "links", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_tags", force: :cascade do |t|
     t.datetime "created_at", null: false
