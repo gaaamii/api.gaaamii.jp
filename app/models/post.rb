0f68ee26b0184e09312ff133ef9ca3b7b3667d35
gaaamii <i1shi4ga4mi1@gmail.com>
2025-12-20 15:42:47 +0900

    fix enum style for Rails 8

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   scope :list, -> { published.order(published_at: :desc).select(:id, :title, :published_at, :status) }
 
-  enum status: { draft: 0, published: 1 }
+  enum :status, { draft: 0, published: 1 }
 end
